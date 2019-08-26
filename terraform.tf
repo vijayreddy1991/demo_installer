bfdcae851a558b575a9dce08e3cdf2102ef618ff
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-08-26 15:28:42 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  access_key  = "${var.accessKey}"
+  secret_key  = "${var.secretKey}"
+  region      = "us-east-2"
+}
+
+resource "aws_instance" "kaeptn-eichhorn" {
+  ami = "ami-0f93b5fd8f220e428"
+  instance_type = "t2.micro"
+
+  }
