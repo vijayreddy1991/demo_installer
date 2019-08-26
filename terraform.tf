68d6278680ab1e792dba956cd022665696019ce3
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-08-26 16:11:15 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region      = "us-east-2"
 }
 
-resource "aws_instance" "kaeptn-eichhorn" {
+resource "aws_instance" "vijay" {
   ami = "ami-0f93b5fd8f220e428"
   instance_type = "t2.micro"
 
