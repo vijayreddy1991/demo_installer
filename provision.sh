330b42fe854872c9eac9529258d0d750079f68fb
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-08-27 15:13:03 +0530

    Create provision.sh

---
+++ provision.sh
@@ -0,0 +1,11 @@
+mkdir terraform
+cd $pwd/terraform
+sudo apt-get update
+sudo apt-get install wget unzip
+wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+sudo unzip ./terraform_0.11.13_linux_amd64.zip -d /usr/local/bin/
+terraform --version
+terraform init
+pwd
+terraform apply -auto-approve
+terraform destroy -auto-approve
