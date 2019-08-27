706a919fd7d524269629ab49dbc393a2724dcf99
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-08-27 16:29:11 +0530

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -7,8 +7,8 @@ sudo apt-get install wget unzip
 wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
 sudo unzip ./terraform_0.11.13_linux_amd64.zip -d /usr/local/bin/
 terraform --version
+popd
 terraform init
 pwd
 terraform apply -auto-approve
 terraform destroy -auto-approve
-popd
