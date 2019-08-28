9c1f900f52e07922a45549e6d20776728ca59f2e
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-08-28 11:33:48 +0530

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,6 +1,4 @@
 pushd $res_repo_vijay_resourcePath
-mkdir terraform
-pushd terraform
 terraform init
 terraform destroy -auto-approve -var-file="/variable.tf"
 terraform destroy -auto-approve -var-file="/variable.tf"
