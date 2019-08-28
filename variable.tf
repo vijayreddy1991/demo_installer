3ee2cdd11f1f7b16eebe71fb71a7b7033553b789
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-08-28 14:38:56 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,10 @@
 # main creds for AWS connection
 variable "accessKey" {
   description = "AWS access key"
+  default = $int_keys_accessKey"	  
 }
 
 variable "secretKey" {
   description = "AWS secert access key"
+  default = "$int_keys_SecretKey"
 }
