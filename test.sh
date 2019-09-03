77c63d17645e5ded9a94a97154240df130c8d940
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-09-03 18:49:38 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,15 @@
+sudo docker ps 
+sudo docker ps | grep nexec
+sudo docker ps | grep cron
+sudo docker ps | grep steptrigger
+sudo docker ps | grep runtrigger
+sudo docker ps | grep pipelinesync
+sudo docker ps | grep www
+sudo docker ps | grep api
+sudo docker log nexec
+sudo docker log cron
+sudo docker log steptrigger
+sudo docker log runtrigger
+sudo docker log pipelinesync
+sudo docker log www
+sudo docker log api
