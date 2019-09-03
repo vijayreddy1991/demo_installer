8e142583169e2796c01f77d56d92ae1f8a00e921
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-09-03 13:16:06 +0530

    Update pipeline_installer.sh

--- pipeline_installer.sh
+++ pipeline_installer.sh
@@ -11,8 +11,11 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce=5:18.09.5~3-0~ubuntu-xenial
+echo "***********************************vijay**********************************"
 sudo wget "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -O /usr/local/bin/docker-compose
+echo "***********************************vijay**********************************"
 sudo chmod +x /usr/local/bin/docker-compose
+echo "***********************************vijay**********************************"
 curl -o installer.tar.gz https://entplus.jfrog.io/artifactory/pipelines-installers/installer/pipelines-0.9.1.tar.gz -u$ART_USERNAME:$ART_API_KEY
 mkdir -p installer && tar -C installer -xvzf installer.tar.gz
 cd installer
