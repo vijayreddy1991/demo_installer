4e209b02b0fd3177a15b76e764323861ffa44a13
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-09-03 16:56:07 +0530

    Update pipeline_installer.sh

--- pipeline_installer.sh
+++ pipeline_installer.sh
@@ -18,7 +18,8 @@ echo "*****************************permissions**********************************
 sudo chmod +x /usr/local/bin/docker-compose
 echo "***************env****************"
 echo "***********************************download**********************************"
-curl -o installer.tar.gz https://entplus.jfrog.io/artifactory/pipelines-installers/installer/pipelines-0.9.1.tar.gz -u"$int_ART_keys_ART_USERNAME":"$int_ART_keys_ART_API_KEY"
+curl -o installer.tar.gz https://bintray.com/jfrog/pipelines/download_file?file_path=installers%2Fpipelines-0.9.1.tar.gz
+# curl -o installer.tar.gz https://entplus.jfrog.io/artifactory/pipelines-installers/installer/pipelines-0.9.1.tar.gz -u"$int_ART_keys_ART_USERNAME":"$int_ART_keys_ART_API_KEY"
 echo "***********************************tar**********************************"
 mkdir -p installer && tar -C installer -xvzf installer.tar.gz
 cd installer
