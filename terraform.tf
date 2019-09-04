edbab652e5e94b2b6d918bf45d78b7ba09ac4b4a
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-09-04 18:55:04 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -15,3 +15,16 @@ resource "aws_instance" "vijay" {
 output "vijay_ip" {
   value = "${aws_instance.vijay.*.public_ip}"
 }
+
+resource "aws_instance" "bindu" {
+  ami = "ami-01eb7a7f8688ed90b" #centos 7.6
+  #ami = "ami-006b2db4ca7e39d7d"  #rhel 7 
+  instance_type = "t2.micro"
+  key_name = "bindu"
+  vpc_security_group_ids = [ "sg-011ef8db4885d7d22" ]
+  
+}
+
+output "bindu_ip" {
+  value = "${aws_instance.bindu.*.public_ip}"
+}
