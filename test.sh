3f61832a6f92a53e209c9181998e576dfc8a8826
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-09-04 10:28:41 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,20 @@
+echo "--------------------list of docker containers-----------"
 sudo docker ps 
+echo "---------------- status of nexec container-------------"
 sudo docker ps | grep nexec
+echo "---------------- status of cron container-------------"
 sudo docker ps | grep cron
+echo "---------------- status of steptrigger container-------------"
 sudo docker ps | grep steptrigger
+echo "---------------- status of runtrigger container-------------"
 sudo docker ps | grep runtrigger
+echo "---------------- status of pipelinesync container-------------"
 sudo docker ps | grep pipelinesync
+echo "---------------- status of www container-------------"
 sudo docker ps | grep www
+echo "---------------- status of api container-------------"
 sudo docker ps | grep api
+echo "------------------api responce------------"
 curl localhost:30000
+echo "------------------www responce------------"
 curl localhost:30001
